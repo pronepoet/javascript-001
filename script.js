ca713f4ef69c305327fc314c172f4a23c6548775
pronepoet <ochiengphillip84@gmail.com>
2020-11-02 15:49:15 +0300

    adding function expression and arguments

--- script.js
+++ script.js
@@ -19,3 +19,45 @@ function checkDriverAge(){
 
 checkDriverAge();
    
+//function expression
+var checkDriverAge2 = function(){
+    var age = prompt("Enter your age: ");
+
+    if (age < 18){
+        alert('Sorry, you are too young to drive this car. Powering off');
+    }
+    else if(age === 18)
+    {
+        alert('Congratulations on your first year of driving. Enjoy the ride!');
+    }
+    else if (age > 18){
+    alert('Powering On. Enjoy the ride!');
+    }
+    else
+    {
+        alert('Please try again!')
+    }
+
+}
+
+//adding arguments
+function checkDriverAge(age){
+
+
+    if (age < 18){
+        return 'Sorry, you are too young to drive this car. Powering off';
+    }
+    else if(age === 18)
+    {
+        return 'Congratulations on your first year of driving. Enjoy the ride!';
+    }
+    else if (age > 18){
+    return 'Powering On. Enjoy the ride!';
+    }
+    else
+    {
+        alert('Please try again!')
+    }
+}
+
+checkDriverAge();
