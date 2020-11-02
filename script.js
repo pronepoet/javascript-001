ebd73cf0e7fd1dbe339286d4c5cc92dd1d2ae757
pronepoet <ochiengphillip84@gmail.com>
2020-11-02 14:43:03 +0300

    adding files

---
+++ script.js
@@ -0,0 +1,16 @@
+var age = prompt("Enter your age: ");
+
+if (age < 18){
+    alert('Sorry, you are too young to drive this car. Powering off');
+}
+else if(age === 18)
+{
+    alert('Congratulations on your first year of driving. Enjoy the ride!');
+}
+else if (age > 18){
+alert('Powering On. Enjoy the ride!');
+}
+else
+{
+    alert('Please try again!')
+}
