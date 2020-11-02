efde2706754671821860affeea155a39c9fb2eb9
pronepoet <ochiengphillip84@gmail.com>
2020-11-02 14:48:57 +0300

    adding function declaration

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-var age = prompt("Enter your age: ");
+function checkDriverAge(){
+    var age = prompt("Enter your age: ");
 
-if (age < 18){
-    alert('Sorry, you are too young to drive this car. Powering off');
+    if (age < 18){
+        alert('Sorry, you are too young to drive this car. Powering off');
+    }
+    else if(age === 18)
+    {
+        alert('Congratulations on your first year of driving. Enjoy the ride!');
+    }
+    else if (age > 18){
+    alert('Powering On. Enjoy the ride!');
+    }
+    else
+    {
+        alert('Please try again!')
+    }
 }
-else if(age === 18)
-{
-    alert('Congratulations on your first year of driving. Enjoy the ride!');
-}
-else if (age > 18){
-alert('Powering On. Enjoy the ride!');
-}
-else
-{
-    alert('Please try again!')
-}
+
+checkDriverAge();
+   
